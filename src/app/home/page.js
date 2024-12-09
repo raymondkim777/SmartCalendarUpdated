a3c4876cd1a3f04f9a265e6e88a2053a808ada50
Raymond Kim :D <raymondkim123456789@gmail.com>
2024-12-09 17:37:39 -0600

    created homepage (route: /home), made header component, imported/slightly edited calendar component, not yet complete

---
+++ src/app/home/page.js
@@ -0,0 +1,16 @@
+import Header from "../header";
+import CalendarWeek from "../calendarweek";
+
+const Home = () => {
+    return (
+        <div className='flex flex-col w-full h-screen overflow-hidden bg-stone-50 font-[family-name:var(--font-geist-sans)] font-semibold'>
+            <Header />
+            {/* Calendar */}
+            <main className="flex flex-col w-full h-fit items-center pt-4 overflow-y-scroll">
+                <CalendarWeek />
+            </main>
+        </div>
+    )
+}
+
+export default Home;
